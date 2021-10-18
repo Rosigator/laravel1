9650a98ae26c6c04f0765b6db5742698088b8eec
hector <heccas@gmail.com>
2021-10-18 12:13:22 +0200

    probando artisan para hacer test

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Route::get('/usuarios', function () {
     return 'Estás viendo los usuarios';
 });
+
+//Una simple cosa de prueba
+Route::get('/usuarios/{id}', function ($id) {
+    $val = intval($id);
+
+    if (is_int($val) && $val > 0) {
+        return 'Estás viendo el perfil del usuario ' . $val;
+    } else {
+        return 'Id de usuario incorrecta';
+    }
+});
