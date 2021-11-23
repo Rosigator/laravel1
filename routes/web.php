062ca7043a6037b7e9259da457822834303038e6
hector <heccas@gmail.com>
2021-11-24 00:50:42 +0100

    leccion rutas acabada

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Esto se lo pongo yo pa que no me raye el que me ponga la clase Route como desconocida en todas las rutas.
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//Método que sirve solo para devolver una vista, mucho más sencillo que el anterior
+Route::view('/', 'welcome');
 
 Route::get('/usuarios', function () {
     return 'Estás viendo los usuarios';
@@ -15,6 +21,10 @@ Route::get('/usuarios', function () {
 Route::get('/usuarios/{id}', function ($id) {
     return 'Mostrando detalles del usuario ' . $id;
 })->where('id', '[0-9]+');
+
+Route::get('usuarios/crear', function () {
+    return 'Creando un usuario';
+});
 
 Route::get('/usuarios/{name}/{nickname?}', function ($name, $nickname = null) {
     $common = 'Bienvenido ' . ucfirst($name) . '. ';
